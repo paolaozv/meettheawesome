fbc953b313c945ab41f44ec483cd7dccb192f4db
“nerdale” <alexandra.neira@gmail.com>
2016-12-01 15:37:56 -0500

    pagina skills arreglo estrellas e iconos

---
+++ dist/js/skills_script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+	$(".star-one").click(function(event){
+		$(".star-one").addClass("star-new-one");
+	});
+	
+	$(".star-two").click(function(event){
+		$(".star-two").addClass("star-new-one");
+	});
+	
+	$(".star-three").click(function(event){
+		$(".star-three").addClass("star-new-one");
+	});
+	
+	$(".star-four").click(function(event){
+		$(".star-four").addClass("star-new-one");
+	});
+	
+	$(".star-five").click(function(event){
+		$(".star-five").addClass("star-new-one");
+	});
+
+
+
+	//TECHNICAL SKILLS
+
+	$(".star-one-d").click(function(event){
+	$(".star-one-d").addClass("star-new-one");
+	});
+	
+	$(".star-two-d").click(function(event){
+	$(".star-two-d").addClass("star-new-one");
+	});
+	
+	$(".star-three-d").click(function(event){
+	$(".star-three-d").addClass("star-new-one");
+	});
+	
+	$(".star-four-d").click(function(event){
+	$(".star-four-d").addClass("star-new-one");
+	});
+	
+	$(".star-five-d").click(function(event){
+	$(".star-five-d").addClass("star-new-one");
+	});
+
+	*/
+
+
