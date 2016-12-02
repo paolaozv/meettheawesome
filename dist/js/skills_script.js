309180018ec3ed71910cd94ad6248f2b76e1d560
“nerdale” <alexandra.neira@gmail.com>
2016-12-01 22:34:34 -0500

    arreglos iconos estrellas

--- dist/js/skills_script.js
+++ dist/js/skills_script.js
@@ -1,49 +1,13 @@
 $(document).ready(function(){
 
-	$(".star-one").click(function(event){
-		$(".star-one").addClass("star-new-one");
-	});
-	
-	$(".star-two").click(function(event){
-		$(".star-two").addClass("star-new-one");
-	});
-	
-	$(".star-three").click(function(event){
-		$(".star-three").addClass("star-new-one");
-	});
-	
-	$(".star-four").click(function(event){
-		$(".star-four").addClass("star-new-one");
-	});
-	
-	$(".star-five").click(function(event){
-		$(".star-five").addClass("star-new-one");
+	$('span').click(function(event){
+		$(this).addClass('star');
+		$('.star').addClass('star-new-one');
+		$(this).prevAll().addClass('star-new-one');
 	});
 
-
-
-	//TECHNICAL SKILLS
-
-	$(".star-one-d").click(function(event){
-	$(".star-one-d").addClass("star-new-one");
-	});
-	
-	$(".star-two-d").click(function(event){
-	$(".star-two-d").addClass("star-new-one");
-	});
-	
-	$(".star-three-d").click(function(event){
-	$(".star-three-d").addClass("star-new-one");
-	});
-	
-	$(".star-four-d").click(function(event){
-	$(".star-four-d").addClass("star-new-one");
-	});
-	
-	$(".star-five-d").click(function(event){
-	$(".star-five-d").addClass("star-new-one");
+	$('span').click(function(event){
+		$(this).nextAll().removeClass('star-new-one');
 	});
 
-	*/
-
-
+});
